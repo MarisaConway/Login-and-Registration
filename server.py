b02195cd5dd8826c8bf5a98a0ecf9f47222e5d20
MarisaConway <marisaconway0111@gmail.com>
2019-02-25 20:21:34 -0500

    Added validations for password

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, session, flash
 from mysqlconn import connectToMySQL
-
+from flask_bcrypt import Bcrypt 
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
+bcrypt = Bcrypt(app)
 @app.route("/")
 def index():
-    mysql= connectToMySQL('registration')
-    people = mysql.query_db('SELECT * FROM people;')
+    db= connectToMySQL('registration')
+    people = db.query_db('SELECT * FROM people;')
     print(people)
     return render_template("index.html", all_people = people)
 
@@ -17,22 +20,46 @@ def create():
     if len(request.form['first_name']) < 1:
         is_valid = False 
         flash("Please enter a first name")
+    if 'first_name'.isalpha() == True:
+        print("All characters are alphabets")
+    
+    else:
+        print("All characters are not alphabets.")
+
     if len(request.form['last_name']) < 1:
         is_valid = False 
         flash("Please enter a last name")
+    if 'last_name'.isalpha() == True:
+        print("All characters are alphabets.") 
+    else:
+        print("All characters are not alphabets.")
+    
     if len(request.form['email']) < 2:
         is_valid = False
         flash("Please enter a valid email")
-    if len(request.form['password']) < 2:
+    elif not EMAIL_REGEX.match(request.form['email']):
+        is_valid = False
+        flash("Invalid email address!")
+    # if (mysql_num_rows)
+
+
+    if len(request.form['password']) < 8:
         flash("Please enter a valid password")
+
+    if request.form['c_password'] != request.form['password']:
+        is_valid = False
+        flash("Password does not match")
+
     
     if is_valid==True:
-        query = "INSERT INTO people (first_name, last_name, email, password, created_on, updated_on) VALUES (%(fn)s, %(ln)s, %(email)s, %(pass)s, NOW(), NOW());"
+        pw_hash = bcrypt.generate_password_hash(request.form['password'])  
+        print(pw_hash) 
+        query = "INSERT INTO people (first_name, last_name, email, password, created_on, updated_on) VALUES (%(fn)s, %(ln)s, %(email)s, %(pass_hash)s, NOW(), NOW());"
         data = {
             "fn": request.form["first_name"],
             "ln": request.form["last_name"],
             "email": request.form["email"],
-            "pass": request.form["password"]
+            "pass_hash" : pw_hash
         }
         db = connectToMySQL('registration')
         flash("Successfully added")
